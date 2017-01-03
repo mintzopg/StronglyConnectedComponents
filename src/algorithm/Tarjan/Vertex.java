35249030a8e69c9ea9fe9c4f9759cc501fcaac8f
Giorgos <mintzopg@users.noreply.github.com>
2017-01-03 20:34:16 +0200

    Update Vertex.java

--- src/algorithm/Tarjan/Vertex.java
+++ src/algorithm/Tarjan/Vertex.java
@@ -1,7 +1,66 @@
 package algorithm.Tarjan;
 
-/**
- * Created by gimin on 12/26/16.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vertex {
+  private String name;
+    private List<Vertex> adjacenyList;
+    private boolean visited;
+    private int lowLink;
+    private Vertex predecessor;
+
+    public Vertex(String name) {
+        this.name = name;
+        adjacenyList = new ArrayList<>();
+    }
+
+    public void addNeighbor(Vertex v) {
+        adjacenyList.add(v);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Vertex> getAdjacenyList() {
+        return adjacenyList;
+    }
+
+    public void setAdjacenyList(List<Vertex> adjacenyList) {
+        this.adjacenyList = adjacenyList;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public int getLowLink() {
+        return lowLink;
+    }
+
+    public void setLowLink(int lowLink) {
+        this.lowLink = lowLink;
+    }
+
+    public Vertex getPredecessor() {
+        return predecessor;
+    }
+
+    public void setPredecessor(Vertex predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
