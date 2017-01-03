56f340225f4cbb287f95ddfae182917aabaf8a03
Giorgos <mintzopg@users.noreply.github.com>
2017-01-03 20:35:13 +0200

    Update TarjanAlgorithm.java

--- src/algorithm/Tarjan/TarjanAlgorithm.java
+++ src/algorithm/Tarjan/TarjanAlgorithm.java
@@ -1,7 +1,61 @@
 package algorithm.Tarjan;
 
-/**
- * Created by gimin on 12/26/16.
- */
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public class TarjanAlgorithm {
+  private Deque<Vertex> stack;
+    private List<Vertex> vertexList;
+    private List<List<Vertex>> connectedComponentsList;
+    private int count;
+
+    public TarjanAlgorithm(List<Vertex> vertexList) {
+        this.vertexList = vertexList;
+        stack = new ArrayDeque<>();
+        connectedComponentsList = new ArrayList<>();
+    }
+
+    public void runAlgorithm() {
+
+        for (Vertex vertex : vertexList) {
+            if (! vertex.isVisited())
+                dfs(vertex);
+        }
+    }
+
+    private void dfs(Vertex vertex) {
+        vertex.setLowLink(count++);
+        vertex.setVisited(true);
+        stack.push(vertex);
+        boolean isComponentRoot = true;
+
+        for (Vertex v : vertex.getAdjacenyList()) {
+            if (! v.isVisited()) dfs(v);
+
+            if (vertex.getLowLink() > v.getLowLink()) {
+                vertex.setLowLink(v.getLowLink());
+                isComponentRoot = false;
+            }
+        }
+
+        if (isComponentRoot) {
+            List<Vertex> component = new ArrayList<>();
+
+            while (true) {
+                Vertex actualVertex = stack.pop();
+                component.add(actualVertex);
+                actualVertex.setLowLink(Integer.MAX_VALUE);
+
+                if (actualVertex.getName().equals(vertex.getName())) break;
+            }
+
+            connectedComponentsList.add(component);
+        }
+    }
+
+    public void printComponents() {
+        System.out.println(connectedComponentsList);
+    }
 }
